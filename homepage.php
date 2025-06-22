53c13650636aa9d878f44c1a9fb0c1eea31e6d29
shanharvs <harvey.shanizza@ue.edu.ph>
2025-06-22 19:02:59 +0800

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -16,22 +16,14 @@ if (!isset($_SESSION['user_data'])) {
   <meta charset="utf-8">
   <title>Welcome Page</title>
 
-  <!-- mobile responsive meta -->
+
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   
-  <!-- ** Plugins Needed for the Project ** -->
-  <!-- Bootstrap -->
   <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
-  <!-- slick slider -->
   <link rel="stylesheet" href="plugins/slick/slick.css">
-  <!-- themefy-icon -->
   <link rel="stylesheet" href="plugins/themify-icons/themify-icons.css">
-
-  <!-- Main Stylesheet -->
   <link href="css/style.css" rel="stylesheet">
-  
-  <!--Favicon-->
   <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
   <link rel="icon" href="images/favicon.ico" type="image/x-icon">
 
@@ -93,17 +85,7 @@ if (!isset($_SESSION['user_data'])) {
 </footer>
 <!-- /footer -->
 
-<!-- jQuery -->
 <script src="plugins/jQuery/jquery.min.js"></script>
-<!-- Bootstrap JS -->
-<script src="plugins/bootstrap/bootstrap.min.js"></script>
-<!-- slick slider -->
-<script src="plugins/slick/slick.min.js"></script>
-<!-- filter -->
-<script src="plugins/shuffle/shuffle.min.js"></script>
-
-<!-- Main Script -->
 <script src="js/script.js"></script>
-
 </body>
 </html>
